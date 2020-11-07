0f92cc703dd3aa594ee9c06ffc02862ba9de0756
Eric Green <egreen@boingo.com>
2020-11-06 19:58:31 -0800

    Support port parameter in run script

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-docker run -d \
--p 8080:8080 \
---name coding_challenge \
-coding_challenge:latest
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    port=8080
+else
+    port=$1
+fi
+
+echo docker run -d -p $port:8080 --name coding_challenge coding_challenge:latest
