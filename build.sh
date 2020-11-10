15103425714ff9433b8b2bf0e433ed57b5bdcf77
Eric Green <egreen@boingo.com>
2020-11-09 16:41:12 -0800

    Push and run docker image from docker hub

--- build.sh
+++ build.sh
@@ -7,6 +7,12 @@ rm -rf ./coding_challenge.log
 rm -rf ./coding_challenge.db
 rm -rf ./codingchallenge
 
+#build and test coding challenge project
 docker run --rm -v "$PWD":/usr/src/codingchallenge -w /usr/src/codingchallenge -e GOOS=linux golang:1.15.3 go build;go test -v ./...
 
+#build docker image
 docker build -t coding_challenge:latest .
+
+#push docker image
+docker tag coding_challenge:latest egreen6464/coding_challenge
+docker push egreen6464/coding_challenge:latest
